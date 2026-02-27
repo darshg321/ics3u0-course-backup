57d09e8e42ee5290e22ea282b33bfee96a75cd14
darshg321 <darshg321@gmail.com>
2026-02-27 12:00:00 -0500

    add lab 00

---
+++ Labs/Lab 00 - Introduction/main.py
@@ -0,0 +1,10 @@
+#-----------------------------------------------------------------------------
+# Name:        Introduction (main.py)
+# Purpose:     Print "Hello World!" in Python
+#
+# Author:      Darsh Gupta
+# Created:     2-Feb-2026
+# Updated:     2-Feb-2026
+#-----------------------------------------------------------------------------
+
+print("Hello World!")
